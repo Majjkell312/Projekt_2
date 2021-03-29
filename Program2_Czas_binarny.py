c458dcc36c5a0556deb46e65a668eedb3e3887c8
Michal Kopczynski <Majjkell312@gmail.com>
2021-03-29 12:11:11 +0200

    Program czas binarny:

--- Program2_Czas_binarny.py
+++ Program2_Czas_binarny.py
@@ -3,12 +3,10 @@ import datetime
 data = datetime.datetime.now()
 hours = data.hour
 minutes = data.minute
-
 print (str(hours)+":"+str(minutes))
 
 hours = bin(hours)[2:]
 minutes = bin(minutes)[2:]
-
 print (str(hours) + ":" + str(minutes))
 
 
